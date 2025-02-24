438d6690354988a31dab58e526321f91f1a83506
kevin20202003 <kchala900@gmail.com>
2025-02-24 15:38:02 -0500

    nuevo

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def ejecutar_predicciones():
         return f"Error interno: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
