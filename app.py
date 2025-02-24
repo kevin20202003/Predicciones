c157d00c0d02103747019bc48e5d1e4926314349
kevin20202003 <kchala900@gmail.com>
2025-02-24 15:17:43 -0500

    app con logs

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+import logging
 from flask import Flask
 import subprocess
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/ejecutar_predicciones', methods=['POST'])
 def ejecutar_predicciones():
     try:
-        # Llamar al script de predicciones directamente
+        # Ejecutar el script y capturar errores
         result = subprocess.run(['python3', 'predicciones.py'], capture_output=True, text=True)
         if result.returncode == 0:
-            return f"Predicciones ejecutadas correctamente. {result.stdout}", 200
+            app.logger.info("Predicciones ejecutadas correctamente")
+            return f"Predicciones ejecutadas correctamente: {result.stdout}", 200
         else:
+            app.logger.error(f"Error al ejecutar el script: {result.stderr}")
             return f"Error al ejecutar el script: {result.stderr}", 500
     except Exception as e:
+        app.logger.error(f"Error interno: {str(e)}")
         return f"Error interno: {str(e)}", 500
 
 if __name__ == "__main__":
