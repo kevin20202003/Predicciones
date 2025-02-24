1cf0557a2e2caed14bf9f8b25ea16d4acd2b369f
kevin20202003 <kchala900@gmail.com>
2025-02-24 14:49:17 -0500

    app

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask
 import subprocess
-import os
 
 app = Flask(__name__)
 
 @app.route('/ejecutar_predicciones', methods=['GET'])
 def ejecutar_predicciones():
     try:
-        # Definir la ruta completa del script si es necesario
-        script_path = os.path.join(os.getcwd(), 'predicciones.py')  # Asegúrate de que la ruta sea correcta
-        
-        # Ejecutar el script predicciones.py
-        result = subprocess.run(['python3', script_path], capture_output=True, text=True)
-        
-        if result.returncode == 0:
-            return "Predicciones ejecutadas correctamente", 200
-        else:
-            return f"Error al ejecutar el script: {result.stderr}", 500
+        # Ejecutar el script en segundo plano
+        subprocess.Popen(['python3', 'predicciones.py'])
+        return "Predicciones están siendo procesadas en segundo plano.", 200
     except Exception as e:
-        return f"Error inesperado: {str(e)}", 500
+        return str(e), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
